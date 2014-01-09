e0ab73400f1dc51f787ce1693be313ebc16673b7
ayesha iqbal <ayesha_iqbal@dell.com>
2014-01-09 05:36:05 +0000

    Refactored all the info and command response messages

---
+++ lib/puppet/provider/powerconnect_messages.rb
@@ -0,0 +1,14 @@
+module Puppet::Provider::Powerconnect_messages
+
+  FIRMWARE_VERSION_EXISTS_INFO="Skipping firmware update as existing firmware version is same as firmware version being applied."
+  CHECK_FIRMWARE_VERSION_DEBUG="Puppet::Provider::powerconnect_firmware:Existing version = %s,Applied version = %s"
+  FIRMWARE_IMAGE_DOWNLOAD_ERROR="Failed to download firmware image to switch"
+  FIRMWARE_UPADTE_DOWNLOAD_DEBUG="Puppet::Provider::powerconnect_firmware:Downloading firmware image to switch"
+  FIRMWARE_UPADTE_SET_BOOTIMAGE_DEBUG="Puppet::Provider::powerconnect_firmware:Setting the next active image on reboot to %s"
+  FIRMWARE_UPADTE_SAVE_CONFIG_INFO="Saving switch configuration before rebooting"
+  FIRMWARE_UPADTE_REBOOT_INFO="Rebooting the switch.Wait for 5 minutes."
+  FIRMWARE_UPDATE_REBOOT_SUCCESSFUL_INFO="Successfully rebooted the switch."
+  FIRMWARE_UPDATE_REBOOT_ERROR="Failed to reboot the switch"
+  FIRMWARE_UPDATE_SUCCESSFUL_INFO="Successfully updated firmware on switch"
+  
+end
---
+++ lib/puppet/provider/powerconnect_responses.rb
@@ -0,0 +1,10 @@
+module Puppet::Provider::Powerconnect_responses
+
+  RESPONSE_IMAGE_DOWNLOAD_SUCCESSFUL="File transfer operation completed successfully"
+  RESPONSE_START_IMAGE_DOWNLOAD="Are you sure you want to start"
+  RESPONSE_SAVE="Are you sure you want to save"
+  RESPONSE_REBOOT="Update bootcode and reset"
+  RESPONSE_SAVE_BEFORE_REBOOT="Are you sure you want to continue"
+  RESPONSE_REBOOT_SUCCESSFUL="Validating boot code from image"
+
+end
