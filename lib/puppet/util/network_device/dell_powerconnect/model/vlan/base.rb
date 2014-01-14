34c9260de663d5a39dcbec085f27dd497cc5ec03
s k pandey <s_k_pandey@dell.com>
2014-01-14 04:16:20 +0000

    Spec file for vlan model. Also, modified "lib\puppet\util\network_device\dell_powerconnect\model\vlan\base.rb" regex

--- lib/puppet/util/network_device/dell_powerconnect/model/vlan/base.rb
+++ lib/puppet/util/network_device/dell_powerconnect/model/vlan/base.rb
@@ -20,7 +20,7 @@ module Puppet::Util::NetworkDevice::Dell_powerconnect::Model::Vlan::Base
     end
 
     base.register_scoped :vlan_name, vlan_scope do
-      match /^\d+\s(\S+)/
+      match /^\d+\s+(\S+)/
       cmd 'show vlan'
       add do |transport, value|
         transport.command("name \"#{value}\"")
