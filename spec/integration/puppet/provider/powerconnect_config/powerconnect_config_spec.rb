9aa1643d52d3e24013312dbf839d6872e1e59279
knvsharishkumar <harish_kakumanu@dell.com>
2014-01-24 17:32:28 +0530

    changes done: incorporating the review comments on rspec 1. Removed hardcoded URL for tftp connection. Externalized it to YML file 2. Added missing validation of tests.

--- spec/integration/puppet/provider/powerconnect_config/powerconnect_config_spec.rb
+++ spec/integration/puppet/provider/powerconnect_config/powerconnect_config_spec.rb
@@ -6,6 +6,8 @@ require 'puppet/util/network_device/dell_powerconnect/device'
 require 'puppet/provider/powerconnect_portchannel/dell_powerconnect'
 require 'pp'
 require 'spec_lib/puppet_spec/deviceconf'
+require 'spec_lib/puppet_spec/integrationfixture'
+include PuppetSpec::Integrationfixture
 include PuppetSpec::Deviceconf
 
 
@@ -17,20 +19,26 @@ describe "Integration test for powerconnect config" do
   before do
     @device = provider_class.device(device_conf['url']) 
   end  
+  
+  #Load Add HBA file
+  switch_config_yml =  YAML.load_file(integrationYML('dell_powerconnect', 'powerconnect_config','switch_config.yml'))
 
+  create_node = switch_config_yml['SwitchConfig1']
+    
   let :switch_config do
     Puppet::Type.type(:powerconnect_config).new(
-    :name => 'config1',
-    :url  => 'tftp://172.152.0.85/s252.scr',
-    :force => 'true',
-    :config_type => 'startup'
+    :name                       => create_node['name'],
+    :url                        => create_node['url'],
+    :force                      => create_node['force'],
+    :config_type                => create_node['config_type']
     )
   end
 
   context 'when applying configuration changes' do 
     it "should apply new configuration" do 
       config_instance = provider_class.new(@device, switch_config.to_hash)
-      config_instance.run(switch_config[:url], switch_config[:config_type], switch_config[:force])
+      result = config_instance.run(switch_config[:url], switch_config[:config_type], switch_config[:force])
+      result.should eq(true)
     end
   end
 
