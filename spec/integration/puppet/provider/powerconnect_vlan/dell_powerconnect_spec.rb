e467953058e96f47d9103e897f1585c1d3ba78e3
sandeepkrpandey <meraretail@gmail.com>
2014-01-24 15:07:45 +0530

    Added more test cases for vlan provider methods in integration testing.

--- spec/integration/puppet/provider/powerconnect_vlan/dell_powerconnect_spec.rb
+++ spec/integration/puppet/provider/powerconnect_vlan/dell_powerconnect_spec.rb
@@ -37,6 +37,17 @@ describe "Integration test for Dell PowerConnect switch Vlan" do
   end
 
   context "when create,rename and delete vlan without any error" do
+    
+    it "should lookup a present powerconnect vlan" do
+      result = provider_class.lookup(@device, '1')
+      result[:ensure]==:present
+    end
+        
+    it "should lookup a absent powerconnect vlan" do
+      result = provider_class.lookup(@device, create_vlan[:name])
+      result[:ensure]==:absent
+    end
+        
     it "should create a powerconnect vlan" do
       former_properties = provider_class.lookup(@device, create_vlan[:name])
       properties = {:vlan_name => create_vlan[:vlan_name],:ensure => create_vlan[:ensure]}
