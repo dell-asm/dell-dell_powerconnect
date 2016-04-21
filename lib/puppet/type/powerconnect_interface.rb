267e6db2fc9259d1fa1f67ede2e21f1d55ab65ab
Shaganti <Prem_Shaganti@AMER.DELL.COM>
2016-04-21 16:20:58 -0500

    ASM-6091: updated propeerties validation to accept empty taggedvlanid

--- lib/puppet/type/powerconnect_interface.rb
+++ lib/puppet/type/powerconnect_interface.rb
@@ -5,7 +5,7 @@ Puppet::Type.newtype(:powerconnect_interface) do
     desc "Name of the interface. Valid values start with Gigabitethernet or Gi or Tengigabitethernet or Te followed by unit/slot/port."
     isnamevar
     validate do |value|
-      unless value =~ /^\A[Gg]igabitethernet\s*\S+$/ or value =~ /Gi\s*\S+$/ or value =~ /[Tt]engigabitethernet\s*\S+$/  or value =~ /[Tt]e\s*\S+$/
+      unless value =~ /^\A[Gg]igabitethernet\s*\S+$/ or value =~ /Gi\s*\S+$/ or value =~ /[Tt]engigabitethernet\s*\S+$/ or value =~ /[Tt]e\s*\S+$/
         raise ArgumentError, "%s is not a valid interface name. Valid interface name should start with 'Gigabitethernet or Gi or Tengigabitethernet or Te' followed by unit/slot/port." % value
       end
     end
@@ -28,41 +28,9 @@ Puppet::Type.newtype(:powerconnect_interface) do
   end
 
   newproperty(:access_vlan) do
-      desc "Configures the VLAN ID when the interface is in access mode."
-      defaultto(:absent)
-      newvalues(:absent, /^\d+$/)
-      validate do |value|
-        return if value == :absent || value.empty?
-        raise ArgumentError, "Invalid vlan list: #{value}" if value.include?(',') && !(value.split(',').size > 0)
-        value.split(',').each do |vlan_value|
-          raise ArgumentError, "Invalid range definition: #{value}" if value.include?('-') && value.split('-').size != 2
-          vlan_value.split('-').each do |vlan|
-            all_valid_characters = vlan =~ /^[0-9]+$/
-            raise ArgumentError, "An invalid VLAN ID #{vlan_value} is entered.All VLAN values must be between 1 and 4094." unless all_valid_characters && vlan.to_i >= 1 && vlan.to_i <= 4094
-          end
-        end
-      end
-    end
-  
-    newproperty(:remove_access_vlan) do
-      desc "Remove the VLAN configured in access mode."
-      defaultto(false)
-      newvalues(false,true)
-      validate do |value|
-        return if value == :absent || value.empty?
-        raise ArgumentError, "Invalid vlan list: #{value}" if value.include?(',') && !(value.split(',').size > 0)
-        value.split(',').each do |vlan_value|
-          raise ArgumentError, "Invalid range definition: #{value}" if value.include?('-') && value.split('-').size != 2
-          vlan_value.split('-').each do |vlan|
-            all_valid_characters = vlan =~ /^[0-9]+$/
-            raise ArgumentError, "An invalid VLAN ID #{vlan_value} is entered.All VLAN values must be between 1 and 4094." unless all_valid_characters && vlan.to_i >= 1 && vlan.to_i <= 4094
-          end
-        end
-      end
-    end
-    
-  newproperty(:tagged_general_vlans) do
-    desc "VLANs to add to a general port. Specify non consecutive VLAN IDs with a comma and no spaces. Use a hyphen to designate a range of VLAN IDs."
+    desc "Configures the VLAN ID when the interface is in access mode."
+    defaultto(:absent)
+    newvalues(:absent, /^\d+$/)
     validate do |value|
       return if value == :absent || value.empty?
       raise ArgumentError, "Invalid vlan list: #{value}" if value.include?(',') && !(value.split(',').size > 0)
@@ -76,7 +44,39 @@ Puppet::Type.newtype(:powerconnect_interface) do
     end
   end
 
-   newproperty(:untagged_general_vlans) do
+  newproperty(:remove_access_vlan) do
+    desc "Remove the VLAN configured in access mode."
+    defaultto(false)
+    newvalues(false, true)
+    validate do |value|
+      return if value == :absent || value.empty?
+      raise ArgumentError, "Invalid vlan list: #{value}" if value.include?(',') && !(value.split(',').size > 0)
+      value.split(',').each do |vlan_value|
+        raise ArgumentError, "Invalid range definition: #{value}" if value.include?('-') && value.split('-').size != 2
+        vlan_value.split('-').each do |vlan|
+          all_valid_characters = vlan =~ /^[0-9]+$/
+          raise ArgumentError, "An invalid VLAN ID #{vlan_value} is entered.All VLAN values must be between 1 and 4094." unless all_valid_characters && vlan.to_i >= 1 && vlan.to_i <= 4094
+        end
+      end
+    end
+  end
+
+  newproperty(:tagged_general_vlans) do
+    desc "VLANs to add to a general port. Specify non consecutive VLAN IDs with a comma and no spaces. Use a hyphen to designate a range of VLAN IDs."
+    validate do |value|
+      return if value == :absent
+      raise ArgumentError, "Invalid vlan list: #{value}" if value.include?(',') && !(value.split(',').size > 0)
+      value.split(',').each do |vlan_value|
+        raise ArgumentError, "Invalid range definition: #{value}" if value.include?('-') && value.split('-').size != 2
+        vlan_value.split('-').each do |vlan|
+          all_valid_characters = vlan =~ /^[0-9]+$/
+          raise ArgumentError, "An invalid VLAN ID #{vlan_value} is entered.All VLAN values must be between 1 and 4094." unless all_valid_characters && vlan.to_i >= 1 && vlan.to_i <= 4094
+        end
+      end
+    end
+  end
+
+  newproperty(:untagged_general_vlans) do
     desc "Untagged VLANs to add to a general port. Specify non consecutive VLAN IDs with a comma and no spaces. Use a hyphen to designate a range of VLAN IDs."
     validate do |value|
       return if value == :absent || value.empty?
@@ -149,7 +149,7 @@ Puppet::Type.newtype(:powerconnect_interface) do
   newproperty(:shutdown) do
     desc "Disable the interface. Default value is 'false'"
     defaultto(false)
-    newvalues(true,false)
+    newvalues(true, false)
   end
 
   newproperty(:add_interface_to_portchannel) do
@@ -166,6 +166,6 @@ Puppet::Type.newtype(:powerconnect_interface) do
   newproperty(:remove_interface_from_portchannel) do
     desc "Remove the interface from the port-channel. Default value is 'false'"
     defaultto(false)
-    newvalues(false,true)
+    newvalues(false, true)
   end
 end
